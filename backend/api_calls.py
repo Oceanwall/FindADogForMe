fb25303a6e4c24ee240f74117676aba201f94447
Matthew Zhao <matthew.zhao@utexas.edu>
2019-02-25 21:53:20 -0600

    Passing CI, setup for trying RESTful APIs

---
+++ backend/api_calls.py
@@ -0,0 +1,19 @@
+# APIS TO DEFIITELY USE
+# Petfinder API
+
+# TheDogAPI
+
+# Dog API
+
+# Eventbrite API
+
+# Meetup API
+
+# National Parks API
+
+
+
+# (Uncertain if going to use)
+# Wolfram Alpha API
+
+# Wikipedia API
