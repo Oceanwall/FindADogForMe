322b8602c597291eb1a3963920a3ce3c90d1649e
Matthew Zhao <matthew.zhao@utexas.edu>
2019-03-17 16:09:15 -0500

    Activity seeding script completed

--- backend/seeding/activity_seeding.py
+++ backend/seeding/activity_seeding.py
@@ -6,10 +6,20 @@ load_dotenv()
 
 PARKS_KEY = os.getenv("NATIONAL_PARKS_API_KEY")
 EVENTBRITE_KEY = os.getenv("EVENTBRITE_API_KEY")
+MEETUP_KEY = os.getenv("MEETUP_API_KEY")
 
 API_URL = "https://api.nps.gov/api/v1/parks?"
 EVENTBRITE_URL = "https://www.eventbriteapi.com/v3/events/search"
 EVENTBRITE_VENUE_URL = "https://www.eventbriteapi.com/v3/venues/"
+MEETUP_URL = "https://api.meetup.com/find/upcoming_events"
+
+# Used by Meetup API
+# Amarillo, El Paso, Lubbock, San Antonio, Austin, Fort Worth, Dallas,
+# Houston, Waco, Corpus Christi
+TEXAS_CITIES = [(35.222, -101.831), (31.7619, -106.485), (33.5779, -101.8552),
+                (29.4241, -98.4936), (30.2672, -97.7431), (32.7555, -97.3308),
+                (32.7767, -96.7970), (29.7604, -95.3698), (31.5493, -97.1467),
+                (27.8006, -97.3964)]
 
 def delete_activities():
     """
@@ -145,6 +155,74 @@ def build_event(info):
     }
 
 
+def get_all_meetups():
+    """
+    TODO
+    """
+    meetup_ids = set()
+    meetups = []
+
+    for city in TEXAS_CITIES:
+        payload = {
+            "text": "dog-friendly",
+            "lat" : city[0],
+            "long": city[1],
+            "radius": "smart",
+            "topic_category": "15046,15892,638,242,1998,25375,933,15672",
+            "excluded_groups": "31296393,1584246,29397646,9548332,1789287",
+            "page": 100,
+            "fields": "featured_photo",
+            "key": MEETUP_KEY
+            }
+        response = requests.get(MEETUP_URL, params=payload)
+        if response.status_code == 200:
+            response_obj = json.loads(response.text)
+            for meetup in response_obj["events"]:
+                if meetup["id"] not in meetup_ids:
+                    meetup_ids.add(meetup["id"])
+                    meetups.append(meetup)
+
+    if (len(meetups) == 0):
+        print("No meetups found, possible error?")
+    return meetups
+
+
+
+def build_meetup(info):
+    """
+    TODO
+    """
+
+    # Active Categories: 15046, 15892, 638, 242, 1998, 25375, 933, 15672
+    # No way to discern categories in returned data, so only requesting active categories.
+
+    # TODO: Figure out a better way to tell if a meetup is free (most meetups only
+    # provide cost information in their description).
+
+    if "description" not in info:
+        return
+
+    meetup = {
+        "id": info["id"],
+        "type": "meetup",
+        "url": info["link"],
+        "name": info["name"],
+        "description": info["description"],
+        "latitude": info["group"]["lat"],
+        "longitude": info["group"]["lon"],
+        "location": info["group"]["localized_location"],
+        "is_active": True,
+        "is_free": False,
+        "image_1": info["featured_photo"]["photo_link"] if "featured_photo" in info else None,
+        "image_2": None,
+        "image_3": None,
+        "image_4": None,
+        "designation": None,
+        "weather": None,
+        "directions": None,
+        "date": info["local_date"],
+    }
+
 
 def main():
     """
@@ -164,6 +242,10 @@ def main():
     print("Events seeded!")
 
     # Meetup
+    meetup_data = get_all_meetups()
+    for meetup in meetup_data:
+        build_meetup(meetup)
+    print("Meetups seeded!")
 
 
 if __name__ == "__main__":
