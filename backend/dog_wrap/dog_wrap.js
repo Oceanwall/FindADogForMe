61256eee7a2038b55803d626ff53ee1bbc8203f6
Matthew Zhao <matthew.zhao@utexas.edu>
2019-03-23 01:35:45 -0500

    More reorganization, completed getDogActivities

--- backend/dog_wrap/dog_wrap.js
+++ backend/dog_wrap/dog_wrap.js
@@ -1,69 +1,42 @@
-const API_URL = "https://api.findadogfor.me/api/"
-const utilities = require("./utilities")
+const utilities = require("./utilities");
+const API_URL = "https://api.findadogfor.me/api/";
 
 /*
  * NOTE: If you're not using a param, then you can simply ignore it.
  * Example:
- *      getShelters(id);
+ *      getShelter(id);
  * However, if you want to use a param, but there are unneeded params in the
  * way, then use undefined to ignore those params.
  * Example:
  *      getBreedDogs('shiba inu', undefined, undefined, undefined, 2);
+ *
+ * NOTE: range parameter default value is 0.5
  */
 
-async function getShelters (id, name, latitude, longitude, range, page) {
-    let queryString = "";
-    if (arguments.length > 0){
-        queryString = utilities.build_query("shelter", id, name, latitude, longitude, range, page);
-    }
-    return utilities.perform_api_call(`${API_URL}shelter${queryString}`);
+/*
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+SHELTERS
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+*/
+
+async function getShelter(id, page) {
+    return utilities.getShelters(id, page);
 }
 
-// Activity id
-async function getActivities (id, name, latitude, longitude, range, page) {
-    let queryString = "";
-    if (arguments.length > 0){
-        queryString = utilities.build_query("activity", id, name, latitude, longitude, range, page);
-    }
-    return utilities.perform_api_call(`${API_URL}activity${queryString}`);
-}
-
-//TODO: Figure out location filter
-async function getBreeds (name, latitude, longitude, range, page) {
-    let queryString = "";
-    if (name) {
-        queryString = utilities.build_query("breed", '', name, latitude, longitude, range, page);
-    }
-    return utilities.perform_api_call(`${API_URL}breed${queryString}`);
-}
-
-//TODO: Is range useful for this method?
-async function getDogs (id, shelter_id, range, page){
-    let queryString = "";
-    if (id != '' && id){
-        queryString = `/${id}`;
-    }else if(shelter_id != '' && shelter_id){
-        return getShelterDogs(shelter_id, page);
-    }else if (page){
-        queryString=`?page=${page}`;
-    }
-    return utilities.perform_api_call(`${API_URL}dog${queryString}`);
-}
-
-// Returns activities close to a certain shelter
+ // Returns activities close to a certain shelter
 async function getShelterActivities(id, range, page){
     if (!id)
-        throw new Error("You must provide a shelter ID.");
+     throw new Error("You must provide a shelter ID.");
 
-    let shelter_info = await getShelters(id);
+    let shelter_info = await utilities.getShelters(id);
     lat = shelter_info.latitude;
     lng = shelter_info.longitude;
-    return getActivities('','',lat, lng, range, page);
+    return utilities.getActivities('','',lat, lng, range, page);
 }
 
 async function getShelterBreeds(id){
     if (!id)
-        throw new Error("You must provide a shelter ID.");
+     throw new Error("You must provide a shelter ID.");
 
     queryObject = {
         name:"shelter_id",
@@ -81,7 +54,7 @@ async function getShelterBreeds(id){
 
     let breed_promises = [];
     for (let breed_name of dog_set)
-        breed_promises.push(getBreeds(breed_name));
+        breed_promises.push(utilities.getBreeds(breed_name));
 
     breed_promises = await Promise.all(breed_promises);
     let breeds = [];
@@ -109,33 +82,50 @@ async function getShelterDogs(id, page){
     return utilities.perform_api_call(`${API_URL}dog${queryString}`);
 }
 
-// TODO: Complete this method
-//function getDogActivities();
+/*
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+DOGS
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+*/
+
+async function getDog(id, page) {
+    return utilities.getDogs(id, page);
+}
 
 // Retrieve the breed information for a particular dog
 async function getDogBreed(id){
-    let dog = await getDogs(id);
-    return await getBreeds(dog.breed);
+    let dog = await utilities.getDogs(id);
+    return await utilities.getBreeds(dog.breed);
 }
 
 // Retrieve the shelter a dog is hosted at
 async function getDogShelter(id){
-    let dog = await getDogs(id);
-    return await getShelters(dog.shelter_id);
+    let dog = await utilities.getDogs(id);
+    return await utilities.getShelters(dog.shelter_id);
 }
 
-function buildBreedActivityQuery(active, queryString, multipleArgs){
-    if (active === true){
-        filter_string = '{"name" : "is_active", "op":"eq", "val": true}'
-    } else {
-        filter_string = '{"name" : "is_active", "op":"eq", "val": false}'
-    }
-    if(multipleArgs){
-        queryString = [queryString.slice(0, 15), filter_string,',', queryString.slice(15)].join('');
-    }else{
-        queryString = '?q={"filters":[' + filter_string + ']}'
-    }
-    return queryString;
+async function getDogActivities(id, range = 0.5, page) {
+    if (!id)
+        throw new Error("You must provide a shelter ID.");
+
+    let dog = await utilities.getDogs(id);
+
+    let breed = await getDogBreed(id);
+    let is_active = breed.is_active;
+
+    let shelter = (await getShelter(dog.shelter_id));
+
+    return await getBreedActivities(breed.name, shelter.latitude, shelter.longitude, range, page);
+}
+
+/*
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+BREEDS
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+*/
+
+async function getBreed(name, page) {
+    return utilities.getBreeds(name, page);
 }
 
 //Returns a activities nearby for a particular breed
@@ -147,49 +137,41 @@ async function getBreedActivities(name, latitude, longitude, range, page) {
         multipleArgs = true;
     }
 
-    let breed_info = await getBreeds(name);
+    let breed_info = await utilities.getBreeds(name);
     let is_active = breed_info.objects[0].is_active;
-    queryString = buildBreedActivityQuery(is_active, queryString, multipleArgs);
+    queryString = utilities.buildBreedActivityQuery(is_active, queryString, multipleArgs);
     return utilities.perform_api_call(`${API_URL}activity${queryString}`);
 }
 
+// TODO: getBreedDogs, getBreedShelters
+
 //TODO: Latitude and Longitude doesn't work as I forgot dogs have no location attribute
 //ONLY USE WITH NAME AND PAGE
-async function getBreedDogs(name, latitude, longitude, range, page){
-    return new Promise (async (resolve, reject) => {
-        var queryString = '';
-        let filter_string = `{"name" : "name", "op":"eq", "val": "${name}"}`;
-        if (arguments.length > 2 && latitude && longitude && latitude != 0 && longitude != 0){
-            queryString = utilities.build_query("breed", '', '', latitude, longitude, range, page);
-            queryString = [queryString.slice(0, 15), filter_string,',', queryString.slice(15)].join('');
-        }else{
-            queryString = "?q=" + filter_string;
-            if(page){
-                queryString+= `&page=${page}`;
-            }
-        }
-        request({
-            url: `${API_URL}dog${queryString}`,
-            method: "GET",
-        }, (error, response, body) => {
-            if(error){
-                reject(error)
-            }else{
-                resolve(JSON.parse(body));
-            }
-        });
-    });
-}
-
-function createPromiseArray(response){
-    var shelterList = [];
-    for(var i = 0; i < response.objects.length; i++){
-        shelterList.push(getShelters(response.objects[i].shelter_id));
-        if(i == response.objects.length - 1){
-            return shelterList;
-        }
-    }
-}
+// async function getBreedDogs(name, latitude, longitude, range, page){
+//     return new Promise (async (resolve, reject) => {
+//         var queryString = '';
+//         let filter_string = `{"name" : "name", "op":"eq", "val": "${name}"}`;
+//         if (arguments.length > 2 && latitude && longitude && latitude != 0 && longitude != 0){
+//             queryString = utilities.build_query("breed", '', '', latitude, longitude, range, page);
+//             queryString = [queryString.slice(0, 15), filter_string,',', queryString.slice(15)].join('');
+//         }else{
+//             queryString = "?q=" + filter_string;
+//             if(page){
+//                 queryString+= `&page=${page}`;
+//             }
+//         }
+//         request({
+//             url: `${API_URL}dog${queryString}`,
+//             method: "GET",
+//         }, (error, response, body) => {
+//             if(error){
+//                 reject(error)
+//             }else{
+//                 resolve(JSON.parse(body));
+//             }
+//         });
+//     });
+// }
 
 // TODO: DOESN'T WORK WITH LOCATION YET
 // Right now it takes way too many calls to get a response as we need to go from
@@ -212,11 +194,24 @@ function createPromiseArray(response){
 //     });
 // }
 
+/*
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ACTIVITIES
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+*/
+
+async function getActivity(id, page) {
+    return utilities.getActivities(id, page);
+}
+
+//TODO: getActivityBreeds, getActivityShelters, getActivityDogs
+
+
 // getShelters().then((response) => {
 //     console.log(response)
 // })
 //
-// getActivities("257930562").then((response) => {
+// utilities.getActivities("257930562").then((response) => {
 //     //console.log(response)
 // })
 //
@@ -228,9 +223,9 @@ function createPromiseArray(response){
 //     console.log(response);
 // });
 //
-getShelterBreeds("TX1399").then((response) => {
-    console.log(response);
-});
+// getShelterBreeds("TX1399").then((response) => {
+//     console.log(response);
+// });
 //
 // getShelterActivities("TX1399", 0.5).then((response) => {
 //     console.log(response);
@@ -238,14 +233,16 @@ getShelterBreeds("TX1399").then((response) => {
 //
 // getShelterDogs("TX1399");
 //
-// getDogs('',"TX1399").then((response) => {
+// utilities.getDogs('',"TX1399").then((response) => {
 //     //console.log(response);
 // })
 //
 // getDogBreed('43022980').then((response) =>{
 //     console.log(response);
 // });
-
+getDogActivities("43022980").then((response) => {
+    console.log(response);
+});
 //
 // getDogShelter('43022980').then((response) => {
 //     console.log(response);
--- backend/dog_wrap/utilities.js
+++ backend/dog_wrap/utilities.js
@@ -1,81 +1,140 @@
 const request = require('request');
+const API_URL = "https://api.findadogfor.me/api/";
+
+function perform_api_call(url) {
+    return new Promise ((resolve, reject) => {
+        request({
+            "url": url,
+            "method": "GET",
+        }, (error, response, body) => {
+            if(error) {
+                reject(new Error('Unable to connect to API'));
+            } else {
+                resolve(JSON.parse(body));
+            }
+        });
+    });
+}
+
+// NOTE: range defaults to 0.5
+function build_query(model, id, name, latitude, longitude, range = 0.5, page) {
+    var queryString = '';
+    // Add case for all params except page
+    if (id && !name && !latitude && !longitude && !page){
+        queryString+= '/' + id;
+    }
+    else if (name && !latitude && !longitude){
+        if(model === "shelter" || model === "breed"){
+            queryObject = {
+                name: "name",
+                op: "eq",
+                val: name,
+            }
+        } else if (model === "activity"){
+            queryObject = {
+                type: "name",
+                op: "eq",
+                val: name,
+            }
+        }
+        queryString = `?q={"filters":[${JSON.stringify(queryObject)}]}`;
+        if (page){
+            queryString += `&page[number]=${page}`;
+        }
+    }
+    else if (latitude && longitude){
+        latitude_lower = {
+            name:"latitude",
+            op:"ge",
+            val: latitude - range
+        };
+        latitude_upper = {
+            name:"latitude",
+            op: "le",
+            val: latitude + range
+        };
+        longitude_lower = {
+            name:"longitude",
+            op:"ge",
+            val: longitude - range
+        };
+        longitude_upper = {
+            name:"longitude",
+            op: "le",
+            val: longitude + range
+        };
+
+        ll = JSON.stringify(latitude_lower);
+        lu = JSON.stringify(latitude_upper);
+        lol = JSON.stringify(longitude_lower);
+        lou = JSON.stringify(longitude_upper);
+
+        queryString = `?q={"filters":[${ll}, ${lu}, ${lol}, ${lou}]}`
+
+        if (page){
+            queryString += `&page[number]=${page}`;
+        }
+
+    }
+    return queryString;
+}
+
+async function getShelters(id, name, latitude, longitude, range, page) {
+    let queryString = "";
+    if (arguments.length > 0){
+        queryString = build_query("shelter", id, name, latitude, longitude, range, page);
+    }
+    return perform_api_call(`${API_URL}shelter${queryString}`);
+}
+
+async function getDogs(id, shelter_id, range, page){
+    let queryString = "";
+    if (id){
+        queryString = `/${id}`;
+    }else if(shelter_id){
+        return getShelterDogs(shelter_id, page);
+    }else if (page){
+        queryString=`?page=${page}`;
+    }
+    return perform_api_call(`${API_URL}dog${queryString}`);
+}
+
+async function getBreeds(name, latitude, longitude, range, page) {
+    let queryString = "";
+    if (name) {
+        queryString = build_query("breed", undefined, name, latitude, longitude, range, page);
+    }
+    return perform_api_call(`${API_URL}breed${queryString}`);
+}
+
+async function getActivities(id, name, latitude, longitude, range, page) {
+    let queryString = "";
+    if (arguments.length > 0){
+        queryString = build_query("activity", id, name, latitude, longitude, range, page);
+    }
+    return perform_api_call(`${API_URL}activity${queryString}`);
+}
+
+function buildBreedActivityQuery(active, queryString, multipleArgs){
+    if (active === true){
+        filter_string = '{"name" : "is_active", "op":"eq", "val": true}'
+    } else {
+        filter_string = '{"name" : "is_active", "op":"eq", "val": false}'
+    }
+    if(multipleArgs){
+        queryString = [queryString.slice(0, 15), filter_string,',', queryString.slice(15)].join('');
+    }else{
+        queryString = '?q={"filters":[' + filter_string + ']}'
+    }
+    return queryString;
+}
 
 module.exports = {
-    perform_api_call: function(url) {
-        return new Promise ((resolve, reject) => {
-            request({
-                "url": url,
-                "method": "GET",
-            }, (error, response, body) => {
-                if(error) {
-                    reject(new Error('Unable to connect to API'));
-                } else {
-                    resolve(JSON.parse(body));
-                }
-            });
-        });
-    },
-    build_query: function(model, id, name, latitude, longitude, range, page) {
-        var queryString = '';
-        // Add case for all params except page
-        if (id && !name && !latitude && !longitude && !range && !page){
-            queryString+= '/' + id;
-        }
-        else if (name && !latitude && !longitude && !range){
-            if(model === "shelter" || model === "breed"){
-                queryObject = {
-                    name: "name",
-                    op: "eq",
-                    val: name,
-                }
-            } else if (model === "activity"){
-                queryObject = {
-                    type: "name",
-                    op: "eq",
-                    val: name,
-                }
-            }
-            queryString = `?q={"filters":[${JSON.stringify(queryObject)}]}`;
-            if (page){
-                queryString += `&page[number]=${page}`;
-            }
-        }
-        else if (latitude && longitude){
-            if (!range)
-                range = 0
-            latitude_lower = {
-                name:"latitude",
-                op:"ge",
-                val: latitude - range
-            };
-            latitude_upper = {
-                name:"latitude",
-                op: "le",
-                val: latitude + range
-            };
-            longitude_lower = {
-                name:"longitude",
-                op:"ge",
-                val: longitude - range
-            };
-            longitude_upper = {
-                name:"longitude",
-                op: "le",
-                val: longitude + range
-            };
-
-            ll = JSON.stringify(latitude_lower);
-            lu = JSON.stringify(latitude_upper);
-            lol = JSON.stringify(longitude_lower);
-            lou = JSON.stringify(longitude_upper);
-
-            queryString = `?q={"filters":[${ll}, ${lu}, ${lol}, ${lou}]}`
-
-            if (page){
-                queryString += `&page[number]=${page}`;
-            }
-
-        }
-        return queryString;
-    }
-};
+    perform_api_call,
+    build_query,
+    getShelters,
+    getDogs,
+    getBreeds,
+    getActivities,
+    buildBreedActivityQuery
+}
